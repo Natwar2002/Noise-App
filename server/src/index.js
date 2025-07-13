4c87a0092cadbc36ad14051f269621604d4f739e
Natwar Patidar <natvarp6062@gmail.com>
2025-07-14 02:38:29 +0530

    Basic Socket.io Setup

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import connectDB from './config/dbConfig.js';
 import apiRouter from './routes/apiRouter.js';
+import { Server } from 'socket.io';
+import { createServer } from 'http';
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
 
 app.use(express.json());
 app.use(express.text());
@@ -16,7 +20,11 @@ app.get('/ping', (req, res) => {
     });
 });
 
-app.listen(3000, async () => {
+io.on('connection', (socket) => {
+    console.log("A user connected", socket);
+})
+
+server.listen(3000, async () => {
     console.log("Server is running on http://localhost:3000");
     connectDB();
 });
